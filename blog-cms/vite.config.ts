14793e4c704771ad9788d1ba64e7ccf90e68dcf9
fuyao-striver <651908384@qq.com>
2026-03-29 17:53:58 +0800

    feat: 添加Element Plus及相关依赖并配置自动导入
    添加Element Plus组件库及相关依赖，包括sass和自动导入插件
    配置vite以支持Element Plus自动导入和组件解析
    添加路径别名和开发服务器代理配置

--- blog-cms/vite.config.ts
+++ blog-cms/vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    // https://cn.vitejs.dev/config/#resolve-alias
+    alias: {
+      // 设置别名
+      "@": path.resolve(__dirname, "./src"),
+    },
+    // https://cn.vitejs.dev/config/#resolve-extensions
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+  },
+  server: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
