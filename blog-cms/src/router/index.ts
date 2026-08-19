bb84882388c3bb3fc0487e9e7567bdd5d530061b
fuyao <651908384@qq.com>
2026-08-19 21:16:37 +0800

    错误页面

--- blog-cms/src/router/index.ts
+++ blog-cms/src/router/index.ts
@@ -1,8 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+
+const constantRouters: RouteRecordRaw[] = [
+  {
+    path: "/:pathMatch(.*)*",
+    component: () => import("@/views/error/404.vue"),
+    meta: {
+      hidden: true,
+    },
+  },
+  {
+    path: "/login",
+    name: "登录",
+    component: () => import("@/views/login/index.vue"),
+    meta: {
+      hidden: true,
+    }
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: constantRouters,
+});
 
-export default router
+export default router;
