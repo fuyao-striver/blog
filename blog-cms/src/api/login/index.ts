aadc2294517e483d346f4c9964bd4f18f30cbcad
fuyao <651908384@qq.com>
2026-08-20 22:20:13 +0800

    仓库工具

---
+++ blog-cms/src/api/login/index.ts
@@ -0,0 +1,18 @@
+import requests from "@/utils/request";
+import type { LoginForm } from "./type";
+import type { AxiosPromise } from "axios";
+import type { Result } from "@/modal";
+
+/**
+  * 用户登录请求
+  *
+  * @param data 用户登录信息
+  * @returns token
+  */
+export const login = (data: LoginForm): AxiosPromise<Result<string>> => {
+  return requests({
+    url: '/login',
+    method: "post",
+    data
+  })
+}
---
+++ blog-cms/src/store/modules/user.ts
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+import type { UserState } from "../interface";
+import type { LoginForm } from "@/api/login/type";
+import { setToken } from "@/utils/token";
+import { login } from "@/api/login";
+
+const useUserStore = defineStore("useUserStore", {
+  state: (): UserState => ({
+    id: null,
+    avatar: "",
+    roleList: [],
+    permissionList: [],
+  }),
+  actions: {
+    LogIn(LoginForm: LoginForm) {
+      return new Promise((resolve, reject) => {
+        login(LoginForm)
+          .then(({ data }) => {
+            if (data.flag) {
+              setToken(data.data);
+              resolve(data);
+            } else {
+              reject(data.msg);
+            }
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    }
+  },
+});
+
+export default useUserStore;
